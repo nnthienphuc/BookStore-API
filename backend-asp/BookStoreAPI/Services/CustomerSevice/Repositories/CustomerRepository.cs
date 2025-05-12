19e6feb5c7e2fd14fe6c29e603a6b29d79bb4036
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2025-05-12 11:07:05 +0700

    Add address into searching for customer

--- backend-asp/BookStoreAPI/Services/CustomerSevice/Repositories/CustomerRepository.cs
+++ backend-asp/BookStoreAPI/Services/CustomerSevice/Repositories/CustomerRepository.cs
@@ -31,7 +31,8 @@ namespace BookStoreAPI.Services.CustomerSevice.Repositories
         public async Task<IEnumerable<Customer>> SearchByKeywordAsync(string keyword)
         {
             return await _context.Customers
-                .Where(c => c.FamilyName.Contains(keyword) || c.GivenName.Contains(keyword) || c.Phone.Contains(keyword))
+                .Where(c => c.FamilyName.Contains(keyword) || c.GivenName.Contains(keyword)
+                || c.Phone.Contains(keyword) || c.Address.Contains(keyword))
                 .ToListAsync();
         }
 
