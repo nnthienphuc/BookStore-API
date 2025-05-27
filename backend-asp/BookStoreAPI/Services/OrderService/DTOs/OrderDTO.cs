edaae3ea725757dc8f04ccea21a461408af21654
unknown <412ngan@gmail.com>
2025-05-27 17:30:11 +0700

    fix get all book, order

--- backend-asp/BookStoreAPI/Services/OrderService/DTOs/OrderDTO.cs
+++ backend-asp/BookStoreAPI/Services/OrderService/DTOs/OrderDTO.cs
@@ -5,8 +5,11 @@
         public Guid Id { get; set; }
         public string StaffName { get; set; } = null!;
         public string CustomerName { get; set; } = null!;
-        public string CustomerPhone { get; set; } = null!;
         public string? PromotionName { get; set; }
+        public Guid StaffId { get; set; }
+        public Guid CustomerId { get; set; }
+        public Guid? PromotionId { get; set; }
+        public string CustomerPhone { get; set; } = null!;
         public DateTime CreatedTime { get; set; }
         public decimal TotalAmount { get; set; }
         public decimal SubTotalAmount { get; set; }
