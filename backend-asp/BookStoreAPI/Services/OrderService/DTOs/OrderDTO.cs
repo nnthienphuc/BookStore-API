6181a50bc54632d46e5a1e513b27d6e931eaf01e
unknown <412ngan@gmail.com>
2025-05-27 16:38:49 +0700

    update order

--- backend-asp/BookStoreAPI/Services/OrderService/DTOs/OrderDTO.cs
+++ backend-asp/BookStoreAPI/Services/OrderService/DTOs/OrderDTO.cs
@@ -4,9 +4,12 @@
     {
         public Guid Id { get; set; }
         public string StaffName { get; set; } = null!;
+        public Guid StaffId { get; set; }
         public string CustomerName { get; set; } = null!;
+        public Guid CustomerId { get; set; } 
         public string CustomerPhone { get; set; } = null!;
         public string? PromotionName { get; set; }
+        public Guid PromotionId { get; set; }
         public DateTime CreatedTime { get; set; }
         public decimal TotalAmount { get; set; }
         public decimal SubTotalAmount { get; set; }
