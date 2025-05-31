2cb7d260b72d908b8fa4ae0fa2d021850f963671
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2025-05-31 11:13:16 +0700

    Run project

--- backend-asp/BookStoreAPI/obj/Debug/net8.0/BookStoreAPI.AssemblyInfo.cs
+++ backend-asp/BookStoreAPI/obj/Debug/net8.0/BookStoreAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edaae3ea725757dc8f04ccea21a461408af21654")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+727f63180872b541ac643c398c08585e7dc221d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
